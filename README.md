9d04a6e465a0706596a8cc6790b816454382320f
r3dhun9 <jasphi168@gmail.com>
2019-10-02 15:34:47 +0800

    add README and search.py

---
+++ README.md
@@ -0,0 +1,21 @@
+![](https://i.imgur.com/J2bfQcR.png)
+
+<div align="center">
+<a href="https://www.facebook.com/philip.chen.581"><img src="https://img.shields.io/badge/author-Redhung-green"></a>
+<img src="https://img.shields.io/pypi/pyversions/pip">
+<a href="https://twitter.com/r3dhun9"><img src="https://img.shields.io/twitter/url?style=social&url=https%3A%2F%2Ftwitter.com%2Fr3dhun9"></a>
+</div>
+
+---
+
+# Description
+An automatic tool searching ip in defined subnet.
+
+Redhung 2019.10.02
+
+# Usage
+```git
+git clone https://github.com/r3dhun9/IPSearch.git
+cd IPSearch
+python search.py
+```
---
+++ search.py
@@ -0,0 +1,97 @@
+import sys
+import subprocess
+import signal
+
+title = """
+
+    ________  _____                      __
+   /  _/ __ \/ ___/___  ____ ___________/ /_
+   / // /_/ /\__ \/ _ \/ __ `/ ___/ ___/ __ \\
+ _/ // ____/___/ /  __/ /_/ / /  / /__/ / / /
+/___/_/    /____/\___/\__,_/_/   \___/_/ /_/
+
+
+An automatic ip search tool by Redhung aka r3dhun9.
+Contact: redhung@hung.red
+"""
+
+ip_list = []
+
+def handler(signal, frame):
+    print("\n[+] IP Search done.")
+    if len(ip_list) != 0:
+        with open("output.txt", "wb") as fd:
+            fd.writelines(ip_list)
+        print("[+] Please check output.txt.")
+    else:
+        print("[-] No IP alive.")
+    sys.exit()
+
+def search_24(ip):
+    signal.signal(signal.SIGINT, handler)
+    for i in range(1, 256):
+        c_ip = ip.split('.')
+        c_ip = c_ip[0] + '.' + c_ip[1] + '.' + c_ip[2] + '.' + str(i)
+        cmd = "ping -c 1 -t 1 " + c_ip
+        p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+        buf = p.stdout.read()
+        if buf.find("from") != -1:
+            print("[!] Found the alive target: " + c_ip)
+            ip_list.append(c_ip)
+
+def search_16(ip):
+    signal.signal(signal.SIGINT, handler)
+    for i in range(1, 256):
+        for j in range(1, 256):
+            c_ip = ip.split('.')
+            c_ip = c_ip[0] + '.' + c_ip[1] + '.' + str(i) + '.' + str(j)
+            cmd = "ping -c 1 -t 1 " + c_ip
+            p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+            buf = p.stdout.read()
+            if buf.find("from") != -1:
+                print("[!] Found the alive target: " + c_ip)
+                ip_list.append(c_ip)
+
+def search_8(ip):
+    signal.signal(signal.SIGINT, handler)
+    for i in range(1, 256):
+        for j in range(1, 256):
+            for k in range(1, 256):
+                c_ip = ip.split('.')
+                c_ip = c_ip[0] + '.' + str(i) + '.' + str(j) + '.' + str(k)
+                cmd = "ping -c 1 -t 1 " + c_ip
+                p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+                buf = p.stdout.read()
+                if buf.find("from") != -1:
+                    print("[!] Found the alive target: " + c_ip)
+                    ip_list.append(c_ip)
+
+def main():
+    print(title)
+    if len(sys.argv) != 3:
+        print("[!] Usage: python search.py <ip> <netmask>")
+        print("[!] E.g. ip: 192.168.0.0, netmask: 255.255.255.0")
+        sys.exit()
+    ip = sys.argv[1]
+    netmask = sys.argv[2]
+    print("[+] Start searching ...")
+    print("[+] You may press Ctrl+C to interrupt the process and get current result.")
+    if netmask == "255.255.255.0":
+        search_24(ip)
+    elif netmask == "255.255.0.0":
+        search_16(ip)
+    elif netmask == "255.0.0.0":
+        search_8(ip)
+    else:
+        print("[-] Error: Netmask out of range.")
+        sys.exit()
+    print("[+] IP Search done.")
+    if len(ip_list) != 0:
+        with open("output.txt", "wb") as fd:
+            fd.writelines(ip_list)
+        print("[+] Please check output.txt.")
+    else:
+        print("[-] No IP alive.")
+
+if __name__ == '__main__':
+    main()
